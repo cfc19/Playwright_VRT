db2576a7727606c179e98c99b77b32cf998bce00
cfc19 <68814150+cfc19@users.noreply.github.com>
2022-03-25 15:22:58 +0000

    Added a test to fill in the form

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,16 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   const title = page.locator('#root > div > header > h1');
-  await expect(title).toHaveText('Hello this is a test');
-  expect(await page.screenshot()).toMatchSnapshot();
+  await expect(title).toHaveText('Test react app');
+  expect(await page.screenshot({fullPage: true })).toMatchSnapshot();
+  expect(await page.textContent('#root > div > header > h1')).toMatchSnapshot('h1.txt');
 });
+
+test('Fill in form and submit',async ({page}) => {
+  await page.goto('/');
+  await page.fill('[data-test="name-input"]', 'Bob')
+  await page.fill('[data-test="age-input"]', '21')
+  expect(await page.screenshot({fullPage: true })).toMatchSnapshot();
+})
