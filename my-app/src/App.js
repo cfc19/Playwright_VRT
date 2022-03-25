21d6933fb6b17d9901851072d849d0eb061ac4c8
cfc19 <68814150+cfc19@users.noreply.github.com>
2022-03-25 15:22:42 +0000

    Updated app to contain a form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+   setSubmitting(true);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 10000)
+ }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello this is a test</h1>
-        <h2>This is a dog</h2>
+        <h1>Test react app</h1>
+        <h2>This is a dummy app for testing only</h2>
+        {submitting &&
+       <div>Submtting Form...</div>
+     }
+        <form onSubmit={handleSubmit}>
+        <fieldset>
+         <label>
+           <p>Please Enter your name</p>
+           <input name="name" data-test="name-input" />
+         </label>
+         <label>
+            <p>Please enter your age</p>
+            <input name="age" data-test="age-input"/>
+         </label>
+       </fieldset>
+       <button data-test="submitBtn" type="submit">Submit</button>
+       </form>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
